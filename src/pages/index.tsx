4602a3f53e04e87e4a6e58c53324e2989028667e
Arno21venter <arno.venter21@gmail.com>
2024-03-25 12:33:53 +0000

    message here

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-// import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
-// import styles from "./index.module.css";
-import styles from "~/styles/styles.module.css"
-import { boolean } from "zod";
-import { useState } from "react";
+import styles from "./index.module.css";
 
 export default function Home() {
-  // const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
-  // const [buttonWasClicked, setButtonWasClicked] = useState(false);
+  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -28,10 +23,10 @@ export default function Home() {
 
 
 
-{/* HMaking a new comment */}
+{/* Hello Arno, how are you? */}
 
 
-
+    
 {/* ===================================================== */}
 
 
@@ -40,59 +35,32 @@ export default function Home() {
 
         <div className={styles.container}>
           <h1 className={styles.title}>
-          Surely Tech
+            Create <span className={styles.pinkSpan}>T3</span> App
           </h1>
           <div className={styles.cardRow}>
-
-
-
-
-{/* = ======================================================= */}
-
-
-
-
-
-{/* ctrl + c (copy)
-ctrl + x (cut)
-ctrl + v (paste) */}
-
-
-
-
-
-
-
-
-
-
-
-{/* <button onClick={() => setButtonWasClicked(prevState => !prevState)}>
-  Click me
-</button> */}
-
-
-
-{/* 
-{buttonWasClicked == true ? (
-
-): <div></div>} */}
-
-
-
-
             <Link
               className={styles.card}
-              href="/homePage"
-              >
-
-
+              href="https://create.t3.gg/en/usage/first-steps"
+              target="_blank"
+            >
               <h3 className={styles.cardTitle}>First Steps →</h3>
-
-
-
-
+              <div className={styles.cardText}>
+                Just the basics - Everything you need to know to set up your
+                database and authentication.
+              </div>
             </Link>
+            <Link
+              className={styles.card}
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className={styles.cardTitle}>Documentation →</h3>
+              <div className={styles.cardText}>
+                Learn more about Create T3 App, the libraries it uses, and how
+                to deploy it.
+              </div>
+            </Link>
+<<<<<<< Updated upstream
 
             <Link
               className={styles.card}
@@ -116,57 +84,41 @@ ctrl + v (paste) */}
 
 
 
+=======
+>>>>>>> Stashed changes
           </div>
-         
+          <div className={styles.showcaseContainer}>
+            <p className={styles.showcaseText}>
+              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            </p>
+            <AuthShowcase />
+          </div>
         </div>
-
-        {/* ================================================================================================ */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* ================================================================================================ */}
       </main>
     </>
   );
 }
 
-// Please remove the below authentication code (Arno)
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
 
-// function AuthShowcase() {
-//   const { data: sessionData } = useSession();
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
 
-//   const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
-
-//   return (
-//     <div className={styles.authContainer}>
-//       <p className={styles.showcaseText}>
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className={styles.loginButton}
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// }
+  return (
+    <div className={styles.authContainer}>
+      <p className={styles.showcaseText}>
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className={styles.loginButton}
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
